f8c294123bc5cca9c6fc32c6ba6b0f4743cc62e8
Milena <100636013+milena579@users.noreply.github.com>
2024-08-15 11:47:54 -0300

    Create script.js

---
+++ ProjetoEstudantes/script.js
@@ -0,0 +1,36 @@
+function mudarFoto(){
+    let inputImg = document.getElementById('botaoImg').click() 
+  
+    let foto = document.getElementById('foto');
+
+}
+
+// let photo = document.getElementById('imgFoto');
+// let file = document.getElementById('flImage');
+
+
+// photo.addEventListener('click', () => {
+//     file.click();
+// });
+
+
+// file.addEventListener('change', () => {
+
+//     // Sem essa verificação, ele irá dar erro quando o usuário clicar em cancelar
+//     // pois enviará uma "imagem" vazia
+//     if (file.files.length == 0) {
+//         return;
+//     }
+
+//     // Inicializando a função que pega o caminho da imagem
+//     let reader = new FileReader();
+
+//     // Está pegando o caminho da imagem
+//     reader.readAsDataURL(file.files[0]);
+    
+//     // Coloca o caminho da imagem no Source da tag IMG
+//     reader.onload = () => {
+//         photo.src = reader.result
+//     }
+
+// });
